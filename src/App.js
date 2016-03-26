18dbc233d6b2c21107b78e600da110915caf45a6
Rewon Child <rewonc@gmail.com>
2016-03-26 00:25:28 -0700

    Get server side rendering working

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ const App = () => (
   <html>
     <head>
       <title>pastalog ~~~~~~~</title>
+      <script src="/socket.io/socket.io.js" defer></script>
+      <script src='/app.js' defer></script>
     </head>
     <body id="container">
       <Container />
     </body>
-    <script src="/socket.io/socket.io.js"></script>
-    <link href="https://npmcdn.com/basscss@8.0.1/css/basscss.min.css" rel="stylesheet" />
-    <script src='/app.js'></script>
   </html>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,82 @@
 // webpack.config.js
-module.exports = {
-  entry: './src/client.js',
-  output: {
-    filename: './dist/assets/app.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015-webpack', 'react'],
+
+// hack to avoid packaging external modules
+// http://jlongster.com/Backend-Apps-with-Webpack--Part-I
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = [
+
+  // CLIENT
+  {
+    entry: './src/client.js',
+    output: {
+      filename: './dist/assets/app.js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015-webpack', 'react'],
+          },
         },
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
-    ],
+        {
+          test: /\.json$/,
+          loader: 'json-loader',
+        },
+      ],
+    },
+    resolve: {
+      // you can now require('file') instead of require('file.coffee')
+      extensions: ['', '.js'],
+    },
   },
-  resolve: {
-    // you can now require('file') instead of require('file.coffee')
-    extensions: ['', '.js'] ,
+
+  // NODE SERVER
+  {
+    entry: './src/server.js',
+    target: 'node',
+    output: {
+      filename: './dist/server.js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015-webpack', 'react'],
+          },
+        },
+        {
+          test: /\.json$/,
+          loader: 'json-loader',
+        },
+      ],
+    },
+    resolve: {
+      // you can now require('file') instead of require('file.coffee')
+      extensions: ['', '.js'],
+    },
+    node: {
+      console: 'empty',
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+    },
+    externals: nodeModules
   },
-};
+];
+
