55378f71e8553eaad606433ae9e871983e99bd26
Rewon Child <rewonc@gmail.com>
2016-04-04 14:56:42 -0700

    Update to post to correct endpoint

--- pastalog/pastalog/__init__.py
+++ pastalog/pastalog/__init__.py
@@ -2,11 +2,12 @@
 The pastalog Log class, which simply sends a POST request to a the server.
 '''
 import requests
+import os
 
 
 class Log(object):
     def __init__(self, url, model_name):
-        self.url = url
+        self.url = os.path.join(url, 'data')
         self.model_name = model_name
 
     def post(self, series_name, value, step):
